4447f87365b9721b6c03e95709995c30f4b5a49a
Evgeny Simonenko <easimonenko@gmail.com>
2022-11-07 20:27:24 +0300

    Cosmetic edits

--- test/users_SUITE.erl
+++ test/users_SUITE.erl
@@ -1,10 +1,9 @@
 -module(users_SUITE).
 
 -export([all/0, init_per_suite/1, end_per_suite/1]).
--export([users_get/1, users_get_by_email/1, users_get_by_id/1, users_post/1]).
--export([users_post_session/1, users_delete_session/1]).
+-export([users_get/1, users_get_by_email/1, users_get_by_id/1, users_post/1, users_delete/1]).
+-export([users_get_session_by_id/1, users_post_session/1, users_delete_session/1]).
 -export([users_patch_password/1]).
--export([users_delete/1]).
 
 init_per_suite(Config) ->
     ok = inets:start(),
@@ -16,9 +15,9 @@ end_per_suite(_Config) ->
     inets:stop().
 
 all() ->
-    [users_get, users_get_by_email, users_get_by_id, users_post,
+    [users_get, users_get_by_email, users_get_by_id, users_post, users_delete,
     users_post_session, users_delete_session,
-    users_patch_password, users_delete].
+    users_patch_password].
 
 users_get(_Config) ->
     {ok, {{"HTTP/1.1", 200, "OK"},
@@ -29,15 +28,14 @@ users_get_by_email(_Config) ->
     {ok, {{"HTTP/1.1", 200, "OK"},
          [_, _, _, {"content-type", "application/json"}],
          _}} = httpc:request(
-                 io_lib:format("http://localhost:8080/api/users?email=~s",
+                 io_lib:format("http://localhost:8080/api/users/?email=~s",
                                ["user1@example.com"])).
 
 users_get_by_id(_Config) ->
     {ok, {{"HTTP/1.1", 200, "OK"},
          [_, _, _, {"content-type", "application/json"}],
          _}} = httpc:request(
-                 io_lib:format("http://localhost:8080/api/users/~i",
-                               [1])).
+                 io_lib:format("http://localhost:8080/api/users/~i", [1])).
 
 users_post(_Config) ->
     {ok, {{"HTTP/1.1", 200, "OK"},
@@ -48,6 +46,20 @@ users_post(_Config) ->
                                     "application/x-www-form-urlencoded",
                                     ""},
                              [], []).
+
+users_delete(_Config) ->
+    {ok, {{"HTTP/1.1", 200, "OK"},
+         [_, _, _, {"content-type", "application/json"}],
+         _}} = httpc:request(delete, {
+                                    io_lib:format("http://localhost:8080/api/users/~i", [1]),
+                                    []},
+                             [], []).
+
+users_get_session_by_id(_Config) ->
+    {ok, {{"HTTP/1.1", 200, "OK"},
+         [_, _, _, {"content-type", "application/json"}],
+         _}} = httpc:request(
+                 io_lib:format("http://localhost:8080/api/users/~i/session", [1])).
 
 users_post_session(_Config) ->
     {ok, {{"HTTP/1.1", 200, "OK"},
@@ -75,12 +87,4 @@ users_patch_password(_Config) ->
                                     [],
                                     "application/x-www-form-urlencoded",
                                     ""},
-                             [], []).
-
-users_delete(_Config) ->
-    {ok, {{"HTTP/1.1", 200, "OK"},
-         [_, _, _, {"content-type", "application/json"}],
-         _}} = httpc:request(delete, {
-                                    io_lib:format("http://localhost:8080/api/users/~i", [1]),
-                                    []},
                              [], []).
